bfa261ea4ece45f7d0cc91a8e97f58c42254da35
Asif <saluasif@gmail.com>
2016-07-31 09:57:03 +0300

    added tests

---
+++ tests/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^businessflow/', include('businessflow.urls')),
+]
